c7d4d0a278650c83afb9b230491323ae7851e87c
Iris <118032108+Irisufus@users.noreply.github.com>
2026-01-19 04:08:26 +0100

    Update to 1.21.11

--- src/client/kotlin/cz/lukynka/throwabletoasts/client/protocol/ModInstalledPacket.kt
+++ src/client/kotlin/cz/lukynka/throwabletoasts/client/protocol/ModInstalledPacket.kt
@@ -4,12 +4,12 @@ import io.netty.buffer.ByteBuf
 import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload
-import net.minecraft.resources.ResourceLocation
+import net.minecraft.resources.Identifier
 
 class ModInstalledPacket(val version: String) : CustomPacketPayload {
 
     companion object {
-        val RESOURCE_LOCATION = ResourceLocation.fromNamespaceAndPath("throwable_toasts", "mod_installed")
+        val RESOURCE_LOCATION = Identifier.fromNamespaceAndPath("throwable_toasts", "mod_installed")
         val TYPE = CustomPacketPayload.Type<ModInstalledPacket>(RESOURCE_LOCATION)
 
         var STREAM_CODEC: StreamCodec<ByteBuf, ModInstalledPacket> = StreamCodec.composite<ByteBuf, ModInstalledPacket, String>(
